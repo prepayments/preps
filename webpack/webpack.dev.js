3f4bf99ae4ab99192790dad6e053f36bbaa4bd59
Edwin Njeru <mailnjeru@gmail.com>
2019-06-11 10:11:58 +0300

    Change application port to 11001

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -22,14 +22,14 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
             context: [
                 '/'
             ],
-            target: `http${options.tls ? 's' : ''}://localhost:8080`,
+            target: `http${options.tls ? 's' : ''}://localhost:11001`,
             secure: false,
             changeOrigin: options.tls
         },{
             context: [
                 '/websocket'
             ],
-            target: 'ws://127.0.0.1:8080',
+            target: 'ws://127.0.0.1:11001',
             ws: true
         }],
         stats: options.stats,
