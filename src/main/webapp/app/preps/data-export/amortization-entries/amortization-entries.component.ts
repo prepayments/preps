e0c51c6d15d8a0d408399f331da04451387a990c
Edwin Njeru <mailnjeru@gmail.com>
2019-06-11 19:39:17 +0300

    Add redirection links to the amortizations export report

--- src/main/webapp/app/preps/data-export/amortization-entries/amortization-entries.component.ts
+++ src/main/webapp/app/preps/data-export/amortization-entries/amortization-entries.component.ts
@@ -3,8 +3,13 @@ import {Subject} from 'rxjs/internal/Subject';
 import {ActivatedRoute, Router} from '@angular/router';
 import {JhiAlertService} from 'ng-jhipster';
 import {NGXLogger} from 'ngx-logger';
-import {HttpErrorResponse} from '@angular/common/http';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {AmortizationReportingService} from 'app/preps/data-export/amortization-entries/amortization-reporting.service';
+import {ServiceOutletsReportingService} from 'app/preps/data-export/service-outlets/service-outlets-reporting.service';
+import {IServiceOutlet, ServiceOutlet} from 'app/shared/model/prepayments/service-outlet.model';
+import {ITransactionAccount} from 'app/shared/model/prepayments/transaction-account.model';
+import {tap} from 'rxjs/operators';
+import {TransactionAccountReportingService} from 'app/preps/data-export/transaction-accounts/transaction-account-reporting.service';
 
 @Component({
     selector: 'gha-amortization-entries',
@@ -19,12 +24,20 @@ export class AmortizationEntriesComponent implements OnInit, OnDestroy {
     amortizationEntries$: any[];
     amortizationEntries: any[];
 
+    // reference list for service outlets
+    serviceOutlets: IServiceOutlet[];
+
+    // reference list for accounts
+    transactionAccounts: ITransactionAccount[];
+
     constructor(
         protected activatedRoute: ActivatedRoute,
         protected router: Router,
         protected jhiAlertService: JhiAlertService,
         protected log: NGXLogger,
-        protected amortizationReportingService: AmortizationReportingService
+        protected amortizationReportingService: AmortizationReportingService,
+        protected transactionAccountsReportingService: TransactionAccountReportingService,
+        protected serviceOutletReportingService: ServiceOutletsReportingService
     ) {
         // Its loaded faster here than on initilization
         this.dtOptions = {
@@ -34,7 +47,7 @@ export class AmortizationEntriesComponent implements OnInit, OnDestroy {
             pageLength: 5,
             processing: true,
             dom: 'Bfrtip',
-            buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+            buttons: ['copy', 'csv', 'excel', 'pdf', 'print', 'colvis']
         };
 
         // Still the only way to preload the js files
@@ -61,35 +74,110 @@ export class AmortizationEntriesComponent implements OnInit, OnDestroy {
                 this.log.info(`Pulled amortization entry items to render with the template...`);
             }
         );
-    }
 
-    /*getEntries(): Observable<EntityArrayResponseType> {
-        return this.http.get<IAmortizationEntry[]>(this.resourceUrl, { observe: 'response' }).pipe(
-            map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res)),
-            tap(res => this.log.info(`Fetched ${res.body.length} amortization entry items from url ${this.resourceUrl}`))
+      this.transactionAccountsReportingService.getEntities()
+        .pipe(tap(res => this.log.info(`Transaction accounts array has been primed with  ${res.body.length} transaction items...`)))
+        .subscribe(
+          (data: HttpResponse<ITransactionAccount[]>) => {
+            this.transactionAccounts = data.body;
+          },
+          err => this.onError(err)
         );
-    }*/
 
-    /*protected convertDateArrayFromServer(res: EntityArrayResponseType): EntityArrayResponseType {
-        if (res.body) {
-            res.body.forEach((amortizationEntry: IAmortizationEntry) => {
-                amortizationEntry.amortizationDate =
-                    amortizationEntry.amortizationDate != null ? moment(amortizationEntry.amortizationDate) : null;
-            });
+      this.serviceOutletReportingService.getEntities().subscribe(
+        (data: HttpResponse<IServiceOutlet[]>) => {
+          this.serviceOutlets = data.body;
+        },
+        err => this.onError(err),
+        () => {
+          this.log.info(`Service outlets array has been primed with ${this.serviceOutlets.length} items`);
         }
-        return res;
-    }*/
-
-    protected onError(errorMessage: string) {
-        // tell the wide-eyed user
-        this.jhiAlertService.error(errorMessage, null, null);
-        // log to console
-        this.log.error(`Error has occured while fetching amortization entries ${errorMessage}`);
+      );
     }
 
-    ngOnDestroy(): void {
-        this.dtTrigger.unsubscribe();
-        this.amortizationEntries = [];
-        this.amortizationEntries$ = [];
+  private onSelectServiceOutletCode(serviceOutletCode: string): void {
+
+    this.log.info(`Navigating service outlet whose code is : ${serviceOutletCode}`);
+
+    const matchingSol: IServiceOutlet = this.serviceOutlets.filter((sol: IServiceOutlet) => {
+      return sol.serviceOutletCode === serviceOutletCode;
+    })[0];
+
+    const solId: number = matchingSol.id;
+
+    this.log.info(`Navigating ...`);
+
+    this.router.navigate(['/service-outlet', solId, 'view']).then(fulfilled => {
+      if (fulfilled) {
+        this.log.info(`successfully navigated to sol # : ${solId}`);
+      } else {
+        this.onError(`Could not find service outlet id: ${solId}`);
+      }
+    });
+  }
+
+  private onSelectAccountNumber(accountNumber: string): void {
+
+    this.log.info(`Navigating Transaction account whose account# is : ${accountNumber}`);
+
+    const matchingAccount: ITransactionAccount = this.transactionAccounts.filter((account: ITransactionAccount) => {
+      return account.accountNumber === accountNumber;
+    })[0];
+
+    if (matchingAccount === undefined) {
+      return;
     }
+
+    const accountId: number = matchingAccount.id;
+
+    this.log.info(`Navigating ...`);
+
+    this.router.navigate(['/transaction-account', accountId, 'view']).then(fulfilled => {
+      if (fulfilled) {
+        this.log.info(`successfully navigated to account# : ${accountId}`);
+      } else {
+        this.onError(`Could not find account#: ${accountId}`);
+      }
+    });
+  }
+
+  private onSelectAccountName(accountName: string): void {
+
+    this.log.info(`Navigating Transaction account whose account name is : ${accountName}`);
+
+    const matchingAccount: ITransactionAccount = this.transactionAccounts.filter((account: ITransactionAccount) => {
+      return account.accountName === accountName;
+    })[0];
+
+    if (matchingAccount === undefined) {
+
+      // early exit
+      return;
+    }
+
+    const accountId: number = matchingAccount.id;
+
+    this.log.info(`Navigating ...`);
+
+    this.router.navigate(['/transaction-account', accountId, 'view']).then(fulfilled => {
+      if (fulfilled) {
+        this.log.info(`successfully navigated to account# : ${accountId}`);
+      } else {
+        this.onError(`Could not find account#: ${accountId}`);
+      }
+    });
+  }
+
+  protected onError(errorMessage: string) {
+      // tell the wide-eyed user
+      this.jhiAlertService.error(errorMessage, null, null);
+      // log to console
+      this.log.error(`Error has occured while fetching amortization entries ${errorMessage}`);
+  }
+
+  ngOnDestroy(): void {
+      this.dtTrigger.unsubscribe();
+      this.amortizationEntries = [];
+      this.amortizationEntries$ = [];
+  }
 }
