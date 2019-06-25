3f368886c323fd580f067850566dabd7ca27247c
Edwin Njeru <mailnjeru@gmail.com>
2019-06-25 19:52:29 +0300

    Implement AmortizationUploadExcelFileDeserializer

--- src/main/java/io/github/prepayments/app/excel/ExcelFileUtils.java
+++ src/main/java/io/github/prepayments/app/excel/ExcelFileUtils.java
@@ -2,17 +2,20 @@ package io.github.prepayments.app.excel;
 
 import com.poiji.option.PoijiOptions.PoijiOptionsBuilder;
 import io.github.prepayments.app.excel.deserializer.AmortizationEntryExcelFileDeserializer;
+import io.github.prepayments.app.excel.deserializer.AmortizationUploadExcelFileDeserializer;
 import io.github.prepayments.app.excel.deserializer.PrepaymentEntryExcelFileDeserializer;
 import io.github.prepayments.app.excel.deserializer.RegisteredSupplierDataEntryFileDeserializer;
 import io.github.prepayments.app.excel.deserializer.ServiceOutletDataEntryFileDeserializer;
 import io.github.prepayments.app.excel.deserializer.TransactionAccountDataEntryExcelFileDeserializer;
 import io.github.prepayments.app.messaging.filing.vm.AmortizationEntryEVM;
+import io.github.prepayments.app.messaging.filing.vm.AmortizationUploadEVM;
 import io.github.prepayments.app.messaging.filing.vm.PrepaymentEntryEVM;
 import io.github.prepayments.app.messaging.filing.vm.RegisteredSupplierEVM;
 import io.github.prepayments.app.messaging.filing.vm.ServiceOutletEVM;
 import io.github.prepayments.app.messaging.filing.vm.TransactionAccountEVM;
 
 import java.time.format.DateTimeFormatter;
+import java.util.BitSet;
 import java.util.List;
 
 /**
@@ -46,6 +49,12 @@ public class ExcelFileUtils {
 
     public static List<TransactionAccountEVM> deserializeTransactionAccountFile(byte[] dataEntryFile) {
         ExcelFileDeserializer<TransactionAccountEVM> excelFileDeserializer = new TransactionAccountDataEntryExcelFileDeserializer(
+            PoijiOptionsBuilder.settings().ignoreHiddenSheets(true).preferNullOverDefault(true).datePattern("yyyy/MM/dd").dateTimeFormatter(DateTimeFormatter.ISO_DATE_TIME).build());
+        return excelFileDeserializer.deserialize(dataEntryFile);
+    }
+
+    public static List<AmortizationUploadEVM> deserializeAmortizationUploadFile(final byte[] dataEntryFile) {
+        ExcelFileDeserializer<AmortizationUploadEVM> excelFileDeserializer = new AmortizationUploadExcelFileDeserializer(
             PoijiOptionsBuilder.settings().ignoreHiddenSheets(true).preferNullOverDefault(true).datePattern("yyyy/MM/dd").dateTimeFormatter(DateTimeFormatter.ISO_DATE_TIME).build());
         return excelFileDeserializer.deserialize(dataEntryFile);
     }
