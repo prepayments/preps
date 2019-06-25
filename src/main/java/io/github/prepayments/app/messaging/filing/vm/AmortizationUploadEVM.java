21c86302666427da39064e9dd3623ccac45a3c2e
Edwin Njeru <mailnjeru@gmail.com>
2019-06-25 19:35:58 +0300

    Add amortization-upload messaging excel veiew model

--- src/main/java/io/github/prepayments/app/messaging/filing/vm/AmortizationUploadEVM.java
+++ src/main/java/io/github/prepayments/app/messaging/filing/vm/AmortizationUploadEVM.java
@@ -26,26 +26,35 @@ public class AmortizationUploadEVM implements Serializable, ExcelViewModel  {
 
     // TODO Provisional fields
     @ExcelCell(0)
-    private String amortizationDate;
-
-    @ExcelCell(1)
-    private String amortizationAmount;
-
-    @ExcelCell(2)
-    private String particulars;
-
-    @ExcelCell(3)
-    private String serviceOutlet;
-
-    @ExcelCell(4)
-    private String accountNumber;
-
-    @ExcelCell(5)
     private String accountName;
 
+    @ExcelCell(1)
+    private String particulars;
+
+    @ExcelCell(2)
+    private String serviceOutlet;
+
+    @ExcelCell(3)
+    private String prepaymentAccountNumber;
+
+    @ExcelCell(4)
+    private String expenseAccountNumber;
+
+    @ExcelCell(5)
+    private String prepaymentTransactionId;
+
     @ExcelCell(6)
-    private String prepaymentEntryId;
+    private String prepaymentTransactionDate;
 
     @ExcelCell(7)
-    private String prepaymentEntryDate;
+    private String prepaymentTransactionAmount;
+
+    @ExcelCell(8)
+    private String amortizationAmount;
+
+    @ExcelCell(9)
+    private String numberOfAmortizations;
+
+    @ExcelCell(10)
+    private String firstAmortizationDate;
 }
