c22b0deaae37c85fcf907b992126a64cf5282c77
Edwin Njeru <mailnjeru@gmail.com>
2019-06-25 20:06:40 +0300

    Add amortization upload fileUploadNotification DTO

---
+++ src/main/java/io/github/prepayments/app/messaging/notifications/dto/AmortizationUploadFileUploadNotification.java
@@ -0,0 +1,24 @@
+package io.github.prepayments.app.messaging.notifications.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * This is a notification object about amortization upload files uploaded. We tried asynch calls but that wasn't good enough. So when a file is uploaded, this object will be sent as notification to
+ * notify a
+ * listener which can then deal with the file.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ToString
+public class AmortizationUploadFileUploadNotification implements FileUploadNotification  {
+
+    private long id;
+    private long timeStamp;
+    private byte[] fileUpload;
+}
