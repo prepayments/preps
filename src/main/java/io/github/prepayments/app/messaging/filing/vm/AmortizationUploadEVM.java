d4b1e8dcff77cf45cad6999bfe324ae1a8d9911d
Edwin Njeru <mailnjeru@gmail.com>
2019-06-26 07:47:36 +0300

    Complete amortization upload EVM definition

--- src/main/java/io/github/prepayments/app/messaging/filing/vm/AmortizationUploadEVM.java
+++ src/main/java/io/github/prepayments/app/messaging/filing/vm/AmortizationUploadEVM.java
@@ -24,7 +24,6 @@ public class AmortizationUploadEVM implements Serializable, ExcelViewModel  {
     @ExcelRow
     private long rowIndex;
 
-    // TODO Provisional fields
     @ExcelCell(0)
     private String accountName;
 
