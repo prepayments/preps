71e48e8d1e157fc69f6d6d05e9e18a8b8453b593
Edwin Njeru <mailnjeru@gmail.com>
2019-06-28 11:24:29 +0300

    Add propagator service to send propagated amortization entries as messages to a queue

--- src/main/java/io/github/prepayments/app/messaging/data_entry/listener/DataSinkAmortizationUploadsStreamsListener.java
+++ src/main/java/io/github/prepayments/app/messaging/data_entry/listener/DataSinkAmortizationUploadsStreamsListener.java
@@ -1,9 +1,8 @@
 package io.github.prepayments.app.messaging.data_entry.listener;
 
+import io.github.prepayments.app.messaging.data_entry.service.AmortizationEntriesPropagatorService;
 import io.github.prepayments.app.messaging.data_entry.vm.SimpleAmortizationUploadEVM;
 import io.github.prepayments.app.messaging.filing.streams.FilingAmortizationUploadStreams;
-import io.github.prepayments.app.messaging.filing.vm.AmortizationEntryEVM;
-import io.github.prepayments.app.messaging.services.AmortizationDataEntryMessageService;
 import io.github.prepayments.service.AmortizationUploadService;
 import io.github.prepayments.service.dto.AmortizationUploadDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -15,18 +14,17 @@ import org.springframework.stereotype.Component;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.stream.IntStream;
 
 @Slf4j
 @Component
 public class DataSinkAmortizationUploadsStreamsListener {
 
     private final AmortizationUploadService amortizationUploadService;
-    private final AmortizationDataEntryMessageService amortizationDataEntryMessageService;
+    private final AmortizationEntriesPropagatorService amortizationEntriesPropagatorService;
 
-    public DataSinkAmortizationUploadsStreamsListener(final AmortizationUploadService amortizationUploadService, final AmortizationDataEntryMessageService amortizationDataEntryMessageService) {
+    public DataSinkAmortizationUploadsStreamsListener(final AmortizationUploadService amortizationUploadService, final AmortizationEntriesPropagatorService amortizationEntriesPropagatorService) {
         this.amortizationUploadService = amortizationUploadService;
-        this.amortizationDataEntryMessageService = amortizationDataEntryMessageService;
+        this.amortizationEntriesPropagatorService = amortizationEntriesPropagatorService;
     }
 
     @StreamListener(FilingAmortizationUploadStreams.INPUT)
@@ -55,40 +53,8 @@ public class DataSinkAmortizationUploadsStreamsListener {
 
         AmortizationUploadDTO result = amortizationUploadService.save(dto);
 
-        propagateAmortizationEntries(simpleAmortizationUploadEVM, dtf, result);
+        amortizationEntriesPropagatorService.propagateAmortizationEntries(dtf, result);
 
         log.debug("Amortization upload item saved : #{}", result.getId());
-    }
-
-    private void propagateAmortizationEntries(final @Payload SimpleAmortizationUploadEVM simpleAmortizationUploadEVM, final AmortizationUploadDTO result) {
-        propagateAmortizationEntries(simpleAmortizationUploadEVM, DateTimeFormatter.ofPattern("yyyy/MM/dd"), result);
-    }
-
-    private void propagateAmortizationEntries(final @Payload SimpleAmortizationUploadEVM simpleAmortizationUploadEVM, final DateTimeFormatter dtf, final AmortizationUploadDTO result) {
-        IntStream.rangeClosed(0, result.getNumberOfAmortizations()-1).forEach((sequence) -> {
-
-            String amortizationDateInstance = incrementDate(simpleAmortizationUploadEVM.getFirstAmortizationDate(), sequence, dtf);
-
-            log.debug("Sending for persistence the amortization instance for the date: {}", amortizationDateInstance);
-
-            amortizationDataEntryMessageService.sendMessage(
-                AmortizationEntryEVM.builder()
-                                    .amortizationDate(amortizationDateInstance)
-                                    .amortizationAmount(
-                                        NumberUtils.toScaledBigDecimal(
-                                            simpleAmortizationUploadEVM.getAmortizationAmount()
-                                                                       .replace(",",""), 2, RoundingMode.HALF_EVEN).toPlainString())
-                                    .particulars(result.getParticulars())
-                                    .serviceOutlet(result.getServiceOutletCode())
-                                    .accountNumber(result.getExpenseAccountNumber())
-                                    .accountName(result.getAccountName())
-                                    .prepaymentEntryId(result.getPrepaymentTransactionId())
-                                    .prepaymentEntryDate(simpleAmortizationUploadEVM.getPrepaymentTransactionDate())
-                                    .build());
-        });
-    }
-
-    private String incrementDate(final String amortizationDate, final int sequence, final DateTimeFormatter dtf) {
-        return dtf.format(LocalDate.parse(amortizationDate, dtf).plusMonths(sequence));
     }
 }
