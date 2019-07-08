36174caca574468d48ca35e59e9c2711ec011b1b
Edwin Njeru <mailnjeru@gmail.com>
2019-07-08 18:42:42 +0300

    Implement routing for prepayment balances links

--- src/main/webapp/app/preps/data-export/data-export-routing.module.ts
+++ src/main/webapp/app/preps/data-export/data-export-routing.module.ts
@@ -1,56 +1,64 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AmortizationEntriesComponent } from './amortization-entries/amortization-entries.component';
 import { PrepaymentEntriesComponent } from './prepayment-entries/prepayment-entries.component';
 import { RegisteredSuppliersComponent } from './registered-suppliers/registered-suppliers.component';
 import { ServiceOutletsComponent } from './service-outlets/service-outlets.component';
 import { TransactionAccountsComponent } from './transaction-accounts/transaction-accounts.component';
-import {PrepaymentsReportResolverService} from 'app/preps/data-export/prepayment-entries/prepayments-report-resolver.service';
+import { PrepaymentsReportResolverService } from 'app/preps/data-export/prepayment-entries/prepayments-report-resolver.service';
+import { PrepaymentBalanceComponent } from 'app/preps/data-export/prepayment-balance/prepayment-balance.component';
 
 const routes: Routes = [
-    {
-        path: 'amortization-entries',
-        component: AmortizationEntriesComponent,
-        data: {
-            pageTitle: 'Export | Amortization Entries',
-            authorities: ['ROLE_USER']
-        }
-    },
-    {
-        path: 'prepayment-entries',
-        component: PrepaymentEntriesComponent,
-        resolve: {
-            prepaymentsResolved: PrepaymentsReportResolverService
-        },
-        data: {
-            pageTitle: 'Export | Prepayment Entries'
-        }
-    },
-    {
-        path: 'registered-suppliers',
-        component: RegisteredSuppliersComponent,
-        data: {
-            pageTitle: 'Export | Registered Suppliers'
-        }
-    },
-    {
-        path: 'service-outlets',
-        component: ServiceOutletsComponent,
-        data: {
-            pageTitle: 'Export | Service Outlets'
-        }
-    },
-    {
-        path: 'transaction-accounts',
-        component: TransactionAccountsComponent,
-        data: {
-            pageTitle: 'Export | Transaction Accounts'
-        }
+  {
+    path: 'amortization-entries',
+    component: AmortizationEntriesComponent,
+    data: {
+      pageTitle: 'Export | Amortization Entries',
+      authorities: ['ROLE_USER']
     }
+  },
+  {
+    path: 'prepayment-entries',
+    component: PrepaymentEntriesComponent,
+    resolve: {
+      prepaymentsResolved: PrepaymentsReportResolverService
+    },
+    data: {
+      pageTitle: 'Export | Prepayment Entries'
+    }
+  },
+  {
+    path: 'registered-suppliers',
+    component: RegisteredSuppliersComponent,
+    data: {
+      pageTitle: 'Export | Registered Suppliers'
+    }
+  },
+  {
+    path: 'service-outlets',
+    component: ServiceOutletsComponent,
+    data: {
+      pageTitle: 'Export | Service Outlets'
+    }
+  },
+  {
+    path: 'transaction-accounts',
+    component: TransactionAccountsComponent,
+    data: {
+      pageTitle: 'Export | Transaction Accounts'
+    }
+  },
+  {
+    path: 'prepayment-balances',
+    component: PrepaymentBalanceComponent,
+    data: {
+      pageTitle: 'Export | Prepayment Balances'
+    }
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class DataExportRoutingModule {}
