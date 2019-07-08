d1f21a9ea11f8d5a2acb1665ceebf6ce7b03b794
Edwin Njeru <mailnjeru@gmail.com>
2019-07-08 16:17:42 +0300

    Add model for prepayment-time-balance

---
+++ src/main/webapp/app/preps/model/prepayment-time-balance.model.spec.ts
@@ -0,0 +1,7 @@
+import { PrepaymentTimeBalance } from './prepayment-time-balance.model';
+
+describe('PrepaymentTimeBalance', () => {
+  it('should create an instance', () => {
+    expect(new PrepaymentTimeBalance()).toBeTruthy();
+  });
+});
