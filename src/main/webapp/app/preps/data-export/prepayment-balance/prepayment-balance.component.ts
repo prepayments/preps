7a98792094f947afa36581df6e51a673f9392509
Edwin Njeru <mailnjeru@gmail.com>
2019-07-08 16:13:22 +0300

    Scaffold prepayment-balance component

--- src/main/webapp/app/preps/data-export/prepayment-balance/prepayment-balance.component.ts
+++ src/main/webapp/app/preps/data-export/prepayment-balance/prepayment-balance.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gha-prepayment-balance',
   templateUrl: './prepayment-balance.component.html',
-  styleUrls: ['./prepayment-balance.component.css']
+  styleUrls: ['./prepayment-balance.component.scss']
 })
 export class PrepaymentBalanceComponent implements OnInit {
   constructor() {}
