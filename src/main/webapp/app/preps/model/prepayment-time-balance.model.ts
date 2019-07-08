1ac086e3b210f7931c0a22d10edc7f3e3b6292f8
Edwin Njeru <mailnjeru@gmail.com>
2019-07-08 18:18:19 +0300

    Implement prepayment-time-balance model for the front-end

--- src/main/webapp/app/preps/model/prepayment-time-balance.model.ts
+++ src/main/webapp/app/preps/model/prepayment-time-balance.model.ts
@@ -1,1 +1,65 @@
-export class PrepaymentTimeBalance {}
+import { Moment } from 'moment';
+
+export interface IPrepaymentTimeBalance {
+  accountName?: string;
+  accountNumber?: string;
+  balanceAmount?: number;
+  id?: number;
+  invoiceNumber?: string;
+  months?: number;
+  particulars?: string;
+  prepaymentAmount?: number;
+  prepaymentDate?: Moment;
+  prepaymentId?: number;
+  scannedDocumentId?: number;
+  serviceOutlet?: string;
+  supplierName?: string;
+}
+
+export class PrepaymentTimeBalance implements IPrepaymentTimeBalance {
+  accountName: string;
+  accountNumber: string;
+  balanceAmount: number;
+  id: number;
+  invoiceNumber: string;
+  months: number;
+  particulars: string;
+  prepaymentAmount: number;
+  prepaymentDate: Moment;
+  prepaymentId: number;
+  scannedDocumentId: number;
+  serviceOutlet: string;
+  supplierName: string;
+
+  constructor(
+    public options: {
+      accountName?: string;
+      accountNumber?: string;
+      balanceAmount?: number;
+      id?: number;
+      invoiceNumber?: string;
+      months?: number;
+      particulars?: string;
+      prepaymentAmount?: number;
+      prepaymentDate?: Moment;
+      prepaymentId?: number;
+      scannedDocumentId?: number;
+      serviceOutlet?: string;
+      supplierName?: string;
+    }
+  ) {
+    this.accountName = options.accountName || '';
+    this.accountNumber = options.accountNumber || '';
+    this.balanceAmount = options.balanceAmount || 0;
+    this.id = options.id || 0;
+    this.invoiceNumber = options.invoiceNumber || '';
+    this.months = options.months || 0;
+    this.particulars = options.particulars || '';
+    this.prepaymentAmount = options.prepaymentAmount || 0;
+    this.prepaymentDate = options.prepaymentDate;
+    this.prepaymentId = options.prepaymentId || 0;
+    this.scannedDocumentId = options.scannedDocumentId || 0;
+    this.serviceOutlet = options.serviceOutlet || '';
+    this.supplierName = options.supplierName || '';
+  }
+}
