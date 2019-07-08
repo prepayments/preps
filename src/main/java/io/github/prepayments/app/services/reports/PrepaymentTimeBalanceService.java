4d477bc0da3ac4f522dbfff6cfe0b6f0b2b01420
Edwin Njeru <mailnjeru@gmail.com>
2019-07-08 15:34:00 +0300

    Implement prepayment-time-balance-service scaffold

--- src/main/java/io/github/prepayments/app/services/reports/PrepaymentTimeBalanceService.java
+++ src/main/java/io/github/prepayments/app/services/reports/PrepaymentTimeBalanceService.java
@@ -1,11 +1,15 @@
 package io.github.prepayments.app.services.reports;
 
 import io.github.prepayments.app.models.PrepaymentTimeBalanceDTO;
+import io.github.prepayments.app.services.AmortizationEntryReportService;
+import io.github.prepayments.app.services.PrepaymentEntryReportService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,11 +18,35 @@ import java.util.List;
 @Service("prepaymentTimeBalanceService")
 public class PrepaymentTimeBalanceService implements ShouldGetBalance<String, PrepaymentTimeBalanceDTO> {
 
+    private final PrepaymentEntryReportService prepaymentEntryReportService;
+    private final AmortizationEntryReportService amortizationEntryReportService;
+    private final OnCallAmortizationService onCallAmortizationService;
+
+    public PrepaymentTimeBalanceService(final PrepaymentEntryReportService prepaymentEntryReportService, final AmortizationEntryReportService amortizationEntryReportService,
+                                        final OnCallAmortizationService onCallAmortizationService) {
+        this.prepaymentEntryReportService = prepaymentEntryReportService;
+        this.amortizationEntryReportService = amortizationEntryReportService;
+        this.onCallAmortizationService = onCallAmortizationService;
+    }
+
     @Override
     public List<PrepaymentTimeBalanceDTO> getBalance(final String balanceDate) {
 
         log.info("Request for prepayment balances for the date: {}", balanceDate);
 
-        return null;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        List<PrepaymentTimeBalanceDTO> prepayments = new ArrayList<>();
+
+        prepaymentEntryReportService.findAll().stream().filter(prep -> {
+            return prep.getPrepaymentDate().isBefore(LocalDate.parse(balanceDate, dtf));
+        }).map(prep -> {
+            return onCallAmortizationService.amortize(LocalDate.parse(balanceDate, dtf), prep, amortizationEntryReportService);
+        }).forEach(prep -> {
+            prepayments.add(prep.findAny().get());
+        });
+
+        // TODO Collectors.collectingAndThen(Collectors.toList(), Collections::<PrepaymentTimeBalanceDTO>unmodifiableList)
+        return prepayments;
     }
 }
