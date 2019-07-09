0da81f01de7f8ab35543a296712c2538fbf2882d
Edwin Njeru <mailnjeru@gmail.com>
2019-07-09 09:12:10 +0300

    Create balance-query-model component scaffold

---
+++ src/main/webapp/app/preps/gha-balance-query/balance-query-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gha-balance-query-modal',
+  templateUrl: './balance-query-modal.component.html',
+  styleUrls: ['./balance-query-modal.component.scss']
+})
+export class BalanceQueryModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/preps/gha-balance-query/gha-balance-query.module.ts
+++ src/main/webapp/app/preps/gha-balance-query/gha-balance-query.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GhaBalanceQueryRoutingModule } from './gha-balance-query-routing.module';
+import { BalanceQueryModalComponent } from './balance-query-modal.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, GhaBalanceQueryRoutingModule]
+  declarations: [BalanceQueryModalComponent],
+  imports: [CommonModule, GhaBalanceQueryRoutingModule],
+  exports: [BalanceQueryModalComponent],
+  entryComponents: [BalanceQueryModalComponent]
 })
 export class GhaBalanceQueryModule {}
