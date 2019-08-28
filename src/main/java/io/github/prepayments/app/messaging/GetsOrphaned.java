1931e753c624baf7fda8f2f1a55755c9bd27cc75
Edwin Njeru <mailnjeru@gmail.com>
2019-08-28 10:56:52 +0300

    Define TagCapable interface

--- src/main/java/io/github/prepayments/app/messaging/GetsOrphaned.java
+++ src/main/java/io/github/prepayments/app/messaging/GetsOrphaned.java
@@ -1,6 +1,9 @@
 package io.github.prepayments.app.messaging;
 
-
+/**
+ * This interface checks if a model which is correlated to another has failed
+ * to establish relationship
+ */
 public interface GetsOrphaned {
 
     boolean orphaned();
