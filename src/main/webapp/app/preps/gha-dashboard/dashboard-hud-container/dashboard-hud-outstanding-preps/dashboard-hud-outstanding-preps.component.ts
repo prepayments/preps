0e95e4dc856548e4e44f915bcbe72890f66789e8
Edwin Njeru <mailnjeru@gmail.com>
2019-09-11 14:34:15 +0300

    Add method to display todays balances

--- src/main/webapp/app/preps/gha-dashboard/dashboard-hud-container/dashboard-hud-outstanding-preps/dashboard-hud-outstanding-preps.component.ts
+++ src/main/webapp/app/preps/gha-dashboard/dashboard-hud-container/dashboard-hud-outstanding-preps/dashboard-hud-outstanding-preps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BalanceQueryModalService } from 'app/preps/gha-balance-query/balance-query-modal.service';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationExtras, Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'gha-dashboard-hud-outstanding-preps',
@@ -10,11 +12,23 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 export class DashboardHudOutstandingPrepsComponent implements OnInit {
   modalRef: NgbModalRef;
 
-  constructor(private balanceQueryModalService: BalanceQueryModalService) {}
+  constructor(private balanceQueryModalService: BalanceQueryModalService, private router: Router) {}
 
   ngOnInit() {}
 
-  navigateToDate(): void {
+  protected navigateToDate(): void {
     this.modalRef = this.balanceQueryModalService.open();
+  }
+
+  protected navigateToDay(): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        balanceDate: moment().format('YYYY-MM-DD'),
+        serviceOutlet: 'All',
+        accountName: 'All'
+      }
+    };
+
+    this.router.navigate(['data-tables/prepayment-balances'], navigationExtras);
   }
 }
