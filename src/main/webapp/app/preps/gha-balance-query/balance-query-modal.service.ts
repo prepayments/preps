52059a35dc601c13444d95e318158a2b1926326e
Edwin Njeru <mailnjeru@gmail.com>
2019-09-13 12:32:35 +0300

    Amend erroneous navigation to amortization-schedule from prepayment-balance-query-modal

--- src/main/webapp/app/preps/gha-balance-query/balance-query-modal.service.ts
+++ src/main/webapp/app/preps/gha-balance-query/balance-query-modal.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { AmortizationScheduleQueryComponent } from 'app/preps/data-display/data-tables/prepayment-amortization/amortization-schedule-query/amortization-schedule-query.component';
+import { BalanceQueryModalComponent } from 'app/preps/gha-balance-query/balance-query-modal.component';
 
 /**
  * This service is used to deploy the BalanceQueryModalComponent as a modal on top of anything
@@ -18,7 +19,7 @@ export class BalanceQueryModalService {
       return;
     }
     this.isOpen = true;
-    const modalRef = this.modalService.open(AmortizationScheduleQueryComponent);
+    const modalRef = this.modalService.open(BalanceQueryModalComponent);
     modalRef.result.then(
       result => {
         this.isOpen = false;
