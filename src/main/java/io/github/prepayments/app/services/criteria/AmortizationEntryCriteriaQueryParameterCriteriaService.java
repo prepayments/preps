4f7492ac4d4188636208f9b796da3af2e8eb3ba7
Edwin Njeru <mailnjeru@gmail.com>
2019-09-13 08:23:21 +0300

    Implement amortization-entry-criteria-query parameter criteria service interface

--- src/main/java/io/github/prepayments/app/services/criteria/AmortizationEntryCriteriaQueryParameterCriteriaService.java
+++ src/main/java/io/github/prepayments/app/services/criteria/AmortizationEntryCriteriaQueryParameterCriteriaService.java
@@ -1,18 +1,63 @@
 package io.github.prepayments.app.services.criteria;
 
+import io.github.jhipster.service.filter.LocalDateFilter;
+import io.github.jhipster.service.filter.StringFilter;
 import io.github.prepayments.app.models.BalanceQuery;
 import io.github.prepayments.app.services.QueryParameterCriteriaService;
+import io.github.prepayments.service.TransactionAccountQueryService;
 import io.github.prepayments.service.dto.AmortizationEntryCriteria;
+import io.github.prepayments.service.dto.TransactionAccountCriteria;
 import org.springframework.stereotype.Component;
+
+import static io.github.prepayments.app.AppConstants.GENERAL_QUERY_REQUEST_ARGS;
+import static io.github.prepayments.app.AppConstants.MONTHLY_AMORTIZATION_DATE;
 
 @Component("amortizationEntryCriteriaQueryParameterCriteriaService")
 public class AmortizationEntryCriteriaQueryParameterCriteriaService implements QueryParameterCriteriaService<BalanceQuery, AmortizationEntryCriteria> {
+
+    private final TransactionAccountQueryService transactionAccountQueryService;
+
+    public AmortizationEntryCriteriaQueryParameterCriteriaService(final TransactionAccountQueryService transactionAccountQueryService) {
+        this.transactionAccountQueryService = transactionAccountQueryService;
+    }
 
     /**
      * Gets criteria of type C given query parameters of type Q
      */
     @Override
     public AmortizationEntryCriteria getCriteria(final BalanceQuery queryParameter) {
-        return null;
+
+        AmortizationEntryCriteria amortizationEntryCriteria = new AmortizationEntryCriteria();
+
+        LocalDateFilter balanceDate = new LocalDateFilter();
+        balanceDate.setEquals(queryParameter.getBalanceDate().withDayOfMonth(MONTHLY_AMORTIZATION_DATE));
+        StringFilter serviceOutletCode = new StringFilter();
+        serviceOutletCode.setEquals(queryParameter.getServiceOutlet());
+        StringFilter prepaymentAccountName = new StringFilter();
+        prepaymentAccountName.setContains(queryParameter.getAccountName());
+
+        // TODO Create account account criteria
+        TransactionAccountCriteria accountCriteria = transactionAccountCriteria(queryParameter.getAccountName());
+        StringFilter prepaymentAccountNumber = new StringFilter();
+        prepaymentAccountNumber.setEquals(transactionAccountQueryService.findByCriteria(accountCriteria).get(0).getAccountNumber());
+
+        // TODO update criteria for data account and service-outlet-code
+        amortizationEntryCriteria.setAmortizationDate(balanceDate);
+        if(!queryParameter.getServiceOutlet().equalsIgnoreCase(GENERAL_QUERY_REQUEST_ARGS)) {
+            amortizationEntryCriteria.setAmortizationServiceOutlet(serviceOutletCode);
+        }
+        if(!queryParameter.getAccountName().equalsIgnoreCase(GENERAL_QUERY_REQUEST_ARGS)) {
+            amortizationEntryCriteria.setPrepaymentAccountNumber(prepaymentAccountNumber);
+        }
+
+        return amortizationEntryCriteria;
+    }
+
+    private TransactionAccountCriteria transactionAccountCriteria(final String accountName) {
+        TransactionAccountCriteria transactionAccountCriteria = new TransactionAccountCriteria();
+        StringFilter accountNameFilter = new StringFilter();
+        accountNameFilter.setContains(accountName);
+        transactionAccountCriteria.setAccountName(accountNameFilter);
+        return transactionAccountCriteria;
     }
 }
