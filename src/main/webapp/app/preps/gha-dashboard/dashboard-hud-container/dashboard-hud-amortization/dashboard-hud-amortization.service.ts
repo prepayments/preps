bcbe7a16dd7e0236f19803bc7b6a21b8d970d85e
Edwin Njeru <mailnjeru@gmail.com>
2019-09-13 18:22:52 +0300

    Scaffold client code to emit amortization card

---
+++ src/main/webapp/app/preps/gha-dashboard/dashboard-hud-container/dashboard-hud-amortization/dashboard-hud-amortization.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { SERVER_API_URL } from 'app/app.constants';
+import { IBalanceQuery } from 'app/preps/model/balance-query.model';
+import { NGXLogger } from 'ngx-logger';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardHudAmortizationService {
+  public resourceAmountUrl = SERVER_API_URL + '/api/reports/balances/prepayments/amount';
+
+  constructor(private http: HttpClient, private log: NGXLogger) {}
+
+  queryAmount(balanceQuery: IBalanceQuery): Observable<number> {
+    return this.http.post<number>(this.resourceAmountUrl, balanceQuery);
+  }
+}
