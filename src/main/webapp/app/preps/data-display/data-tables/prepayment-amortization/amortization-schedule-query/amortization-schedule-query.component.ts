d322d16147a269ce7e106e4248107ae9f07abbdd
Edwin Njeru <mailnjeru@gmail.com>
2019-09-13 14:04:13 +0300

    Update for amortization-query-params with dynamic forms and card style

--- src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/amortization-schedule-query/amortization-schedule-query.component.ts
+++ src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/amortization-schedule-query/amortization-schedule-query.component.ts
@@ -1,78 +1,121 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { RouteStateService } from 'app/preps/route-state.service';
 import { BalanceQuery, IBalanceQuery } from 'app/preps/model/balance-query.model';
-import { FormBuilder, Validators } from '@angular/forms';
-import { JhiAlertService } from 'ng-jhipster';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BalanceQueryModelQuestionService } from 'app/preps/gha-balance-query/balance-query-model-question.service';
+import { NGXLogger } from 'ngx-logger';
+import { QuestionBase } from 'app/preps/model/question-base.model';
 
 @Component({
   selector: 'gha-amortization-schedule-query',
   templateUrl: './amortization-schedule-query.component.html',
   styleUrls: ['./amortization-schedule-query.component.scss']
 })
-export class AmortizationScheduleQueryComponent implements OnInit, AfterViewInit {
+export class AmortizationScheduleQueryComponent implements OnInit, AfterViewInit, OnDestroy {
   private pageUrl = 'data-tables/amortization-schedule';
 
+  questions: QuestionBase<any>[];
+  balanceQuery: IBalanceQuery;
   isSaving: boolean;
 
-  balanceQuery: IBalanceQuery;
-
   editForm = this.fb.group({
-    amortizationDate: [null, [Validators.required]],
-    prepaymentServiceOutlet: [null, [Validators.required]],
-    prepaymentAccountNumber: [null, [Validators.required]]
+    balanceDate: [null, [Validators.required]],
+    accountName: [],
+    serviceOutlet: []
   });
 
-  constructor(
-    protected jhiAlertService: JhiAlertService,
-    private fb: FormBuilder,
-    private router: Router,
-    private modalService: NgbModal,
-    private routeStateService: RouteStateService<IBalanceQuery>
-  ) {}
-
-  ngOnInit() {}
-
-  ngAfterViewInit(): void {
-    // TODO dismiss all
-    this.modalService.dismissAll();
-  }
-
-  navigateToSchedule() {
-    this.balanceQuery = this.createFromForm();
-
-    this.routeStateService.data = this.balanceQuery;
-
-    // TODO Navigate
-    this.router
-      .navigateByUrl(this.pageUrl)
-      .catch()
-      .then()
-      .finally();
-  }
-
-  private createFromForm(): IBalanceQuery {
+  private createFromForm(queryForm: FormGroup): IBalanceQuery {
     return new BalanceQuery({
-      balanceDate: this.editForm.get(['balanceDate']).value,
-      serviceOutlet: this.editForm.get(['serviceOutlet']).value,
-      accountName: this.editForm.get(['accountName']).value
+      balanceDate: queryForm.get(['balanceDate']).value,
+      accountName: queryForm.get(['accountName']).value,
+      serviceOutlet: queryForm.get(['serviceOutlet']).value
     });
   }
 
-  protected onSaveSuccess() {
+  constructor(
+    private fb: FormBuilder,
+    private log: NGXLogger,
+    private router: Router,
+    private stateService: RouteStateService<IBalanceQuery>,
+    // close modal if data entered has error
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit() {
     this.isSaving = false;
-    this.previousState();
+    this.balanceQuery = {};
+    this.questions = BalanceQueryModelQuestionService.getQuestions();
+    this.log.debug(`Entering query for date of balance ...`);
   }
 
-  protected onSaveError() {
-    this.isSaving = false;
+  ngAfterViewInit(): void {}
+
+  ngOnDestroy(): void {
+    this.questions = [];
+    this.balanceQuery = undefined;
   }
-  protected onError(errorMessage: string) {
-    this.jhiAlertService.error(errorMessage, null, null);
+
+  updateBalanceQuery(queryForm: FormGroup) {
+    this.balanceQuery = this.createFromForm(queryForm);
+    this.log.debug(`Balance query updated for date : ${this.balanceQuery.balanceDate}`);
+
+    this.enquire(this.balanceQuery);
+  }
+
+  enquire(balanceQuery: IBalanceQuery) {
+    this.log.debug(
+      `Navigating to balances for date : ${balanceQuery.balanceDate}; service outlet: ${balanceQuery.serviceOutlet}; account name: ${
+        balanceQuery.accountName
+      }`
+    );
+
+    this.stateService.data = balanceQuery;
+    // TODO handle input errors
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        balanceDate: balanceQuery.balanceDate,
+        serviceOutlet: balanceQuery.serviceOutlet,
+        accountName: balanceQuery.accountName
+      }
+    };
+
+    const navigation = this.router.navigate(['data-tables/amortization-schedule'], navigationExtras);
+
+    this.reviewNavigation(navigation, balanceQuery);
+  }
+
+  private reviewNavigation(navigation: any, balanceQuery: IBalanceQuery): void {
+    // note success
+    navigation.then(() => {
+      this.log.debug(`Navigation to '${balanceQuery} completed successfully`);
+      this.modalService.dismissAll('Data table navigation complete!!!');
+    });
+
+    // catch unfortunate navigation incidences
+    navigation.catch(() => {
+      this.log.debug(`Navigation to '${balanceQuery} failed navigating back to previous view...`);
+      this.previousState();
+    });
+
+    // clean up mmodals
+    navigation.finally(() => {
+      this.log.debug(`Navigation to '${balanceQuery} completed successfully`);
+      this.modalService.dismissAll('In case dismissal has failed');
+    });
   }
 
   previousState() {
     window.history.back();
+  }
+
+  private onSaveSuccess() {
+    this.isSaving = false;
+    this.previousState();
+  }
+
+  private onSaveError() {
+    this.isSaving = false;
   }
 }
--- src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/prepayment-amortization.module.ts
+++ src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/prepayment-amortization.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrepaymentAmortizationRoutingModule } from './prepayment-amortization-routing.module';
@@ -6,11 +6,29 @@ import { AmortizationScheduleComponent } from './amortization-schedule.component
 import { DataTablesModule as DTModule } from 'angular-datatables';
 import { AmortizationScheduleQueryComponent } from './amortization-schedule-query/amortization-schedule-query.component';
 import { PrepsSharedModule } from 'app/shared';
+import { PrepsMaterialModule } from 'app/preps/preps-material.module';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { QuestionnaireModule } from 'app/preps/questionnaire/questionnaire.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AmortizationScheduleComponent, AmortizationScheduleQueryComponent],
-  imports: [PrepsSharedModule, CommonModule, PrepaymentAmortizationRoutingModule, DTModule],
+  imports: [
+    QuestionnaireModule,
+    PrepsMaterialModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot({
+      validationMessages: [{ name: 'required', message: 'This field is required' }]
+    }),
+    FormlyMaterialModule,
+    PrepsSharedModule,
+    CommonModule,
+    PrepaymentAmortizationRoutingModule,
+    DTModule
+  ],
   exports: [AmortizationScheduleComponent, AmortizationScheduleQueryComponent],
-  entryComponents: [AmortizationScheduleComponent, AmortizationScheduleQueryComponent]
+  entryComponents: [AmortizationScheduleComponent, AmortizationScheduleQueryComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PrepaymentAmortizationModule {}
