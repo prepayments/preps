6b92b7309e7fdc6df05b48f15b6c5d9eee28af35
Edwin Njeru <mailnjeru@gmail.com>
2019-09-13 12:19:08 +0300

    Create query from manually generated form

--- src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/amortization-schedule-query/amortization-schedule-query.component.ts
+++ src/main/webapp/app/preps/data-display/data-tables/prepayment-amortization/amortization-schedule-query/amortization-schedule-query.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouteStateService } from 'app/preps/route-state.service';
-import { IBalanceQuery } from 'app/preps/model/balance-query.model';
+import { BalanceQuery, IBalanceQuery } from 'app/preps/model/balance-query.model';
 import { FormBuilder, Validators } from '@angular/forms';
 import { JhiAlertService } from 'ng-jhipster';
 import { AmortizationEntry, IAmortizationEntry } from 'app/shared/model/prepayments/amortization-entry.model';
@@ -19,18 +19,9 @@ export class AmortizationScheduleQueryComponent implements OnInit {
   balanceQuery: IBalanceQuery;
 
   editForm = this.fb.group({
-    id: [],
     amortizationDate: [null, [Validators.required]],
-    amortizationAmount: [null, [Validators.required]],
-    particulars: [],
     prepaymentServiceOutlet: [null, [Validators.required]],
-    prepaymentAccountNumber: [null, [Validators.required]],
-    amortizationServiceOutlet: [null, [Validators.required]],
-    amortizationAccountNumber: [null, [Validators.required]],
-    originatingFileToken: [],
-    amortizationTag: [],
-    orphaned: [],
-    prepaymentEntryId: []
+    prepaymentAccountNumber: [null, [Validators.required]]
   });
 
   constructor(
@@ -48,24 +39,19 @@ export class AmortizationScheduleQueryComponent implements OnInit {
     // TODO update service data this.routeStateService.data = //
 
     // TODO Navigate
-    this.router.navigateByUrl(this.pageUrl);
+    this.router
+      .navigateByUrl(this.pageUrl)
+      .catch()
+      .then()
+      .finally();
   }
 
-  private createFromForm(): IAmortizationEntry {
+  private createFromForm(): IBalanceQuery {
     const entity = {
-      ...new AmortizationEntry(),
-      id: this.editForm.get(['id']).value,
+      ...new BalanceQuery(),
       amortizationDate: this.editForm.get(['amortizationDate']).value,
-      amortizationAmount: this.editForm.get(['amortizationAmount']).value,
-      particulars: this.editForm.get(['particulars']).value,
       prepaymentServiceOutlet: this.editForm.get(['prepaymentServiceOutlet']).value,
-      prepaymentAccountNumber: this.editForm.get(['prepaymentAccountNumber']).value,
-      amortizationServiceOutlet: this.editForm.get(['amortizationServiceOutlet']).value,
-      amortizationAccountNumber: this.editForm.get(['amortizationAccountNumber']).value,
-      originatingFileToken: this.editForm.get(['originatingFileToken']).value,
-      amortizationTag: this.editForm.get(['amortizationTag']).value,
-      orphaned: this.editForm.get(['orphaned']).value,
-      prepaymentEntryId: this.editForm.get(['prepaymentEntryId']).value
+      prepaymentAccountNumber: this.editForm.get(['prepaymentAccountNumber']).value
     };
     return entity;
   }
