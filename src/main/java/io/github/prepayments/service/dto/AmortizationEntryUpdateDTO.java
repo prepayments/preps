cb024df60a05048eb127fe6385b191349b5b4f59
Edwin Njeru <mailnjeru@gmail.com>
2019-10-02 12:32:49 +0300

    Simplify amortization-entry-update-dto

--- src/main/java/io/github/prepayments/service/dto/AmortizationEntryUpdateDTO.java
+++ src/main/java/io/github/prepayments/service/dto/AmortizationEntryUpdateDTO.java
@@ -1,4 +1,13 @@
 package io.github.prepayments.service.dto;
+
+import io.github.prepayments.app.messaging.data_entry.updates.InstallableUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
 import java.time.LocalDate;
 import javax.validation.constraints.*;
 import java.io.Serializable;
@@ -8,7 +17,13 @@ import java.util.Objects;
 /**
  * A DTO for the {@link io.github.prepayments.domain.AmortizationEntryUpdate} entity.
  */
-public class AmortizationEntryUpdateDTO implements Serializable {
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ToString
+@Accessors(chain = true)
+public class AmortizationEntryUpdateDTO implements Serializable, InstallableUpdate<Long, AmortizationEntryUpdateDTO> {
 
     private Long id;
 
@@ -44,148 +59,13 @@ public class AmortizationEntryUpdateDTO implements Serializable {
 
     private Boolean orphaned;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getAmortizationEntryId() {
+    @Override
+    public Long getEntryId() {
         return amortizationEntryId;
     }
 
-    public void setAmortizationEntryId(Long amortizationEntryId) {
-        this.amortizationEntryId = amortizationEntryId;
-    }
-
-    public LocalDate getAmortizationDate() {
-        return amortizationDate;
-    }
-
-    public void setAmortizationDate(LocalDate amortizationDate) {
-        this.amortizationDate = amortizationDate;
-    }
-
-    public BigDecimal getAmortizationAmount() {
-        return amortizationAmount;
-    }
-
-    public void setAmortizationAmount(BigDecimal amortizationAmount) {
-        this.amortizationAmount = amortizationAmount;
-    }
-
-    public String getParticulars() {
-        return particulars;
-    }
-
-    public void setParticulars(String particulars) {
-        this.particulars = particulars;
-    }
-
-    public String getPrepaymentServiceOutlet() {
-        return prepaymentServiceOutlet;
-    }
-
-    public void setPrepaymentServiceOutlet(String prepaymentServiceOutlet) {
-        this.prepaymentServiceOutlet = prepaymentServiceOutlet;
-    }
-
-    public String getPrepaymentAccountNumber() {
-        return prepaymentAccountNumber;
-    }
-
-    public void setPrepaymentAccountNumber(String prepaymentAccountNumber) {
-        this.prepaymentAccountNumber = prepaymentAccountNumber;
-    }
-
-    public String getAmortizationServiceOutlet() {
-        return amortizationServiceOutlet;
-    }
-
-    public void setAmortizationServiceOutlet(String amortizationServiceOutlet) {
-        this.amortizationServiceOutlet = amortizationServiceOutlet;
-    }
-
-    public String getAmortizationAccountNumber() {
-        return amortizationAccountNumber;
-    }
-
-    public void setAmortizationAccountNumber(String amortizationAccountNumber) {
-        this.amortizationAccountNumber = amortizationAccountNumber;
-    }
-
-    public Long getPrepaymentEntryId() {
-        return prepaymentEntryId;
-    }
-
-    public void setPrepaymentEntryId(Long prepaymentEntryId) {
-        this.prepaymentEntryId = prepaymentEntryId;
-    }
-
-    public String getOriginatingFileToken() {
-        return originatingFileToken;
-    }
-
-    public void setOriginatingFileToken(String originatingFileToken) {
-        this.originatingFileToken = originatingFileToken;
-    }
-
-    public String getAmortizationTag() {
-        return amortizationTag;
-    }
-
-    public void setAmortizationTag(String amortizationTag) {
-        this.amortizationTag = amortizationTag;
-    }
-
-    public Boolean isOrphaned() {
-        return orphaned;
-    }
-
-    public void setOrphaned(Boolean orphaned) {
-        this.orphaned = orphaned;
-    }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        AmortizationEntryUpdateDTO amortizationEntryUpdateDTO = (AmortizationEntryUpdateDTO) o;
-        if (amortizationEntryUpdateDTO.getId() == null || getId() == null) {
-            return false;
-        }
-        return Objects.equals(getId(), amortizationEntryUpdateDTO.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
-
-    @Override
-    public String toString() {
-        return "AmortizationEntryUpdateDTO{" +
-            "id=" + getId() +
-            ", amortizationEntryId=" + getAmortizationEntryId() +
-            ", amortizationDate='" + getAmortizationDate() + "'" +
-            ", amortizationAmount=" + getAmortizationAmount() +
-            ", particulars='" + getParticulars() + "'" +
-            ", prepaymentServiceOutlet='" + getPrepaymentServiceOutlet() + "'" +
-            ", prepaymentAccountNumber='" + getPrepaymentAccountNumber() + "'" +
-            ", amortizationServiceOutlet='" + getAmortizationServiceOutlet() + "'" +
-            ", amortizationAccountNumber='" + getAmortizationAccountNumber() + "'" +
-            ", prepaymentEntryId=" + getPrepaymentEntryId() +
-            ", originatingFileToken='" + getOriginatingFileToken() + "'" +
-            ", amortizationTag='" + getAmortizationTag() + "'" +
-            ", orphaned='" + isOrphaned() + "'" +
-            "}";
+    public AmortizationEntryUpdateDTO getEntity() {
+        return this;
     }
 }
